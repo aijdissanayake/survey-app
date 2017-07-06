d7b8eec7329c3a87c337e13d69b7e522406db616
Achala Dissanayake <AchalaD@99x.lk>
2017-07-06 21:10:54 +0530

    encoded URI

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,5 @@
 var axios = require('axios');
+var request = require('superagent');
 
 module.exports = {
 
@@ -12,10 +13,11 @@ module.exports = {
 	},
 
 	fetchSurveys: function() {
-		var encodedURI = window.encodeURI("https://script.google.com/macros/s/AKfycbySehYgZd1ftj316wdNYQCchQ8GtTUZaTzQsmroosPX0kLY050/exec");
+		var encodedURI = window.encodeURI("https://script.google.com/macros/s/AKfycbySehYgZd1ftj316wdNYQCchQ8GtTUZaTzQsmroosPX0kLY050/exec?noo=nooo");
 		var i = 0;
 
-		return axios.get(encodedURI).then(function(response){			
+		return axios.get(encodedURI).then(function(response){
+			console.log(response);			
 			var areas = response.data.areas;
 			var areaTitles = [];
 			for (i = 0; i < areas.length; i++){
@@ -56,25 +58,77 @@ module.exports = {
 
 	submitQuiz: function(){
 
+		var encodedURI = window.encodeURI("https://script.google.com/macros/s/AKfycbzyc2CG9xLM-igL3zuslSmNY2GewL5seTWpMpDIQr_5eCod7_U/exec");
+
+		var array = JSON.stringify({
+  		"title": "Digital Marketing",
+  		"choices":[[true,true,true],[true, true, true],[true, true, true]]
+		});
+		var url = 'https://script.google.com/macros/s/AKfycbzyc2CG9xLM-igL3zuslSmNY2GewL5seTWpMpDIQr_5eCod7_U/exec?resp=' + encodeURIComponent(array);
+
+		return axios.get(encodedURI).then(function(response){
+			console.log(response);
+		});
+
 		// axios.post('https://script.google.com/macros/s/AKfycbzyc2CG9xLM-igL3zuslSmNY2GewL5seTWpMpDIQr_5eCod7_U/exec', {
 		//     "ti": 'Fred',
 		//     "lastName": 'Flintstone'
 		//   });
 
-		 
-		axios({
-		  method:'POST',
-		  url:'https://script.google.com/macros/s/AKfycbzyc2CG9xLM-igL3zuslSmNY2GewL5seTWpMpDIQr_5eCod7_U/exec',
-		  data: {
-		    "ti": 'Fred',
-		    "lastName": 'Flintstone'
-		  }
-		}).then(function (response) {
-		    console.log(response);
-		  })
-		  .catch(function (error) {
-		    console.log(error);
-		  });
+		// fetch(encodedURI, {  
+		//     method: 'post',  
+		//     headers: {  
+		//       "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"  
+		//     },  
+		//     body: 'foo=bar&lorem=ipsum'  
+		//   })
+		//   .then(json)  
+		//   .then(function (data) {  
+		//     console.log('Request succeeded with JSON response', data);  
+		//   })  
+		//   .catch(function (error) {  
+		//     console.log('Request failed', error);  
+		//   });
+
+		 //  fetch(encodedURI,
+			// {
+			//     headers: {
+			//       'Accept': 'application/json',
+			//       'Content-Type': 'application/json'
+			//     },
+			//     method: "POST",
+			//     body: JSON.stringify({title: 1, b: 2})
+			// })
+			// .then(function(res){ console.log(res) })
+			// .catch(function(res){ console.log(res)});
+
+			// request
+			//    .post(encodedURI)
+			//    .send({ title: 'Manny', species: 'cat' })
+			//    .set('X-API-Key', 'foobar')
+			//    .set('Accept', 'application/json')
+			//    .end(function(err, res){
+			//      if (err || !res.ok) {
+			//        alert('Oh no! error');
+			//      } else {
+			//        alert('yay got ' + JSON.stringify(res.body));
+			//      }
+			//    });
+
+		
+		// axios({
+		//   method:'POST',
+		//   url:encodedURI,
+		//   data: {
+		//     "ti": 'Fred',
+		//     "lastName": 'Flintstone'
+		//   }
+		// }).then(function (response) {
+		//     console.log(response);
+		//   })
+		//   .catch(function (error) {
+		//     console.log(error);
+		//   });
 
 		// axios.get('https://script.google.com/macros/s/AKfycbzyc2CG9xLM-igL3zuslSmNY2GewL5seTWpMpDIQr_5eCod7_U/exec')
 		//   .then(function (response) {
@@ -88,29 +142,3 @@ module.exports = {
 
 	}     
 }
-
-// fetchSurveys: function() {
-// 		var encodedURI = window.encodeURI("http://localhost:3000/survey");
-
-// 		return axios.get(encodedURI).then(function(response){
-// 			return response.data.areas;
-// 		});
-
-// 	},
-
-// <ul className = 'surveys'>
-//   			{surveys.map(function(survey){
-//   				return(
-//   					<li 
-//   					style = {survey === this.state.selectedSurvey ? { color: 'orange'} : null }
-//   					onClick= {this.updateSurvey.bind(null,survey)}	    				
-//   					key={survey}>
-
-//   					{survey}
-
-//   					</li>
-//   					)
-//   			},this)}
-//   			</ul>\
-//<Route path = '/:survey' component = {(props) => <Quiz area="UX"/>} />
-// <Link className = 'button' to={{ pathname: '/survey', area: "Digital Marketing" }} >survey</Link>
